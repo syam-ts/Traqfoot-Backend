948955d1089059ac776a343fa83b08db263508d7
syam-ts <syamnandhu3@gmail.com>
2025-07-12 16:22:55 +0530

    fix: complete user login

--- src/infrastructure/repository-db/UserRepositoryDb.ts
+++ src/infrastructure/repository-db/UserRepositoryDb.ts
@@ -1,33 +1,34 @@
 import { User, UserModel } from "../../domain/entities/User";
 import { UserRepository } from "../../domain/interfaces/repositories/UserRespository";
-import bcrypt from 'bcryptjs';
+import bcrypt from "bcryptjs";
 
 export class UserRepositoryDb implements UserRepository {
-
-
-    async newUser(infrastructure_name: string, email: string, mobile: number, password: string, since: number): Promise<any> {
-       console.log('appafod', password)
-       const encryptPassword = await bcrypt.hashSync(password, 10); 
+    async newUser(
+        infrastructure_name: string,
+        email: string,
+        mobile: number,
+        password: string,
+        since: number
+    ): Promise<any> {
+        const encryptPassword = await bcrypt.hashSync(password, 10);
 
         const user = await new UserModel({
             infrastructure_name,
             email,
             mobile,
             password: encryptPassword,
-            since
+            since,
         }).save();
 
-        if(!user) throw new Error('Failed creating new user');
+        if (!user) throw new Error("Failed creating new user");
         return user;
     }
 
     async loginUser(email: string, password: string): Promise<any> {
-        console.log('emil', password)
-        const findUser = await UserModel.findOne({email}).lean<any>().exec();
- 
-        console.log('pass 2;', findUser.password)
-        if(!findUser) throw new Error('User not found');
-        if(!bcrypt.compareSync(password, findUser.password)) throw new Error('Wrong password');
+        const findUser = await UserModel.findOne({ email }).lean<any>().exec();
+        if (!findUser) throw new Error("User not found");
+        if (!bcrypt.compareSync(password, findUser.password))
+            throw new Error("Wrong password");
         return findUser;
     }
 }
