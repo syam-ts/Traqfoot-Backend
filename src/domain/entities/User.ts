55cb7c138f79e1df4668ba8e77dff8f3f480e923
syam-ts <syamnandhu3@gmail.com>
2025-07-12 11:57:27 +0530

    feat: add user

--- src/domain/entities/User.ts
+++ src/domain/entities/User.ts
@@ -1,8 +1,11 @@
+import { model, Schema } from "mongoose";
 
+const UserSchema = new Schema({
+    infrastructure_name: { type: String, reqrired: true },
+    email: { type: String, reqrired: true },
+    mobile: { type: Number, reqrired: true },
+    password: { type: String, reqrired: true },
+    since: { type: Number, reqrired: true },
+});
 
-export abstract class User {
-    constructor(name: string) { };
-
-    abstract getRole(): string;
-    abstract viewAllJobPost(): any;
-};
+export const UserModel = model("user", UserSchema);
