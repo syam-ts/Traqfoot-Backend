43197e738c7c8abf8ae38417a79c0a4a2da82b0b
syam-ts <syamnandhu3@gmail.com>
2025-07-13 17:01:09 +0530

    fix: update counter update with missing timestamp

--- src/cron/updateFootfallCount.ts
+++ src/cron/updateFootfallCount.ts
@@ -1,33 +1,32 @@
- import cron from 'node-cron';
-import dotenv from 'dotenv';
-import { SensorModel } from '../domain/entities/Sensor';
+import cron from "node-cron";
+import dotenv from "dotenv";
+import { SensorModel } from "../domain/entities/Sensor";
 
 dotenv.config();
- 
-cron.schedule('0 * * * *', async () => {
-  console.log('🕐 Running hourly sensor count updater...');
 
-  try {
-    const sensors = await SensorModel.find();
+cron.schedule("0 * * * *", async () => {
+    console.log("🕐 Running hourly sensor count updater...");
 
-    const bulkUpdates = sensors.map((sensor) => {
-      const randomIncrement = Math.floor(Math.random() * 500);  
+    try {
+        const sensors = await SensorModel.find();
 
-      return {
-        updateOne: {
-          filter: { _id: sensor._id },
-          update: { $inc: { count: randomIncrement } }
+        const bulkUpdates = sensors.map((sensor) => {
+            const randomIncrement = Math.floor(Math.random() * 500);
+
+            return {
+                updateOne: {
+                    filter: { _id: sensor._id },
+                    update: { $inc: { count: randomIncrement }, timestamp: Date.now() },
+                },
+            };
+        });
+
+        if (bulkUpdates.length > 0) {
+            await SensorModel.bulkWrite(bulkUpdates);
+        } else {
+            console.log("⚠️ No sensors found to update");
         }
-      };
-    });
-
-    if (bulkUpdates.length > 0) {
-      await SensorModel.bulkWrite(bulkUpdates);
-    //   console.log(`✅ Updated ${bulkUpdates.length} sensors with random increments`);
-    } else {
-      console.log('⚠️ No sensors found to update');
+    } catch (err) {
+        console.error("❌ Error during sensor count update:", err);
     }
-  } catch (err) {
-    console.error('❌ Error during sensor count update:', err);
-  }
 });
